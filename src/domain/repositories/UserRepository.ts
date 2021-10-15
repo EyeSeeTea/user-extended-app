9ba1215228d7d76755c4af9697da6a6b33d49bb5
Jocelyn Dunkley <jocelyn.dunkley09@gmail.com>
2021-10-15 08:32:39 +0200

    went through all of actions and added new repository interfaces

--- src/domain/repositories/UserRepository.ts
+++ src/domain/repositories/UserRepository.ts
@@ -6,7 +6,21 @@ export interface UserRepository {
     getCurrent(): FutureData<User>;
     list(): FutureData<PaginatedResponse<User>>;
     getById(id: string): FutureData<User>;
-    replicateFromTemplate(count: number, usernameTemplate: string, passwordTemplate: string): FutureData<User[]>; 
+    replicateFromTemplate(count: number, usernameTemplate: string, passwordTemplate: string): FutureData<User[]>;
+     // newUserFields: [{id, username, password, name, email}]
+    replicateFromTable(newUserFields: string[]): FutureData<User[]>;
     update(users: string[]): FutureData<User[]>;
     save(users: string[]): FutureData<User[]>;
+    delete(users: string[]): FutureData<void>;
+    copyInUser(parents: string[], selectedIds: string[], copyAccessElements: CopyAccessElements, updateStrategy: string):FutureData<User[]>;
+    //idk what type allChildren is 
+    saveUserGroups(parents: string[], allChildren: string[], selectedIds: string[], updateStrategy: string):FutureData<User[]>;
+    saveUserRoles(parents: string[], allChildren: string[], selectedIds: string[], updateStrategy: string):FutureData<User[]>;
+    assignToOrgUnits(userIds: string[], field: string, titleKey: string):FutureData<User[]>;
+}
+interface CopyAccessElements {
+    userGroups: boolean;
+    userRoles: boolean;
+    orgUnitOutput: boolean;
+    orgUnitCapture: boolean;
 }
