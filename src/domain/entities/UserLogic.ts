91dcc412ca4abfff9ac5378f7f8d4738b2e707b2
nshandra <34254522+nshandra@users.noreply.github.com>
2024-12-16 12:36:12 +0100

    feat: add generateRandomPassword function

--- src/domain/entities/UserLogic.ts
+++ src/domain/entities/UserLogic.ts
@@ -14,4 +14,59 @@ export class UserLogic extends Struct<User>() {
             user => user.organisationUnits.length > 0 && user.userRoles.length > 0 && user.userGroups.length > 0
         );
     }
+
+    /**
+     * Generates a random password with a specified length.
+     *
+     * Use the generated password as a temporary one that the user must change after logging in.
+     *
+     * The password will contain at least one character from each of the following categories:
+     * - Lowercase letters
+     * - Uppercase letters
+     * - Numbers
+     * - Special characters
+     *
+     * @param {number} [length=16] - The length of the generated password. Defaults to 16 if not specified.
+     * @returns {string} The generated random password.
+     */
+    static generateRandomPassword(length = 16): string {
+        const charset = {
+            lower: "abcdefghijklmnopqrstuvwxyz",
+            upper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+            number: "0123456789",
+            special: "!@#$%^&*()_+~`|}{[]:;?><,./-=",
+        };
+
+        const getRandomNumber = () => {
+            const array = new Uint32Array(1);
+            crypto.getRandomValues(array);
+            return array[0] as number;
+        };
+
+        const getRandomChar = (str: string): string => {
+            const rand = getRandomNumber();
+            const char = str.charAt(rand % str.length);
+            return char;
+        };
+
+        const requiredChars = [
+            getRandomChar(charset.lower),
+            getRandomChar(charset.upper),
+            getRandomChar(charset.number),
+            getRandomChar(charset.special),
+        ];
+
+        const allChars = charset.lower + charset.upper + charset.number + charset.special;
+        const password = Array.from({ length: length - requiredChars.length }, () => getRandomChar(allChars)).concat(
+            requiredChars
+        );
+
+        const shuffledPassword: string = password
+            .map(char => ({ char, rand: getRandomNumber() % length }))
+            .sort((a, b) => a.rand - b.rand)
+            .map(({ char }) => char)
+            .join("");
+
+        return shuffledPassword;
+    }
 }
