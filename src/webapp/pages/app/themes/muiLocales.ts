97f759435cf4a5d67a93b72db9c158ea18ef0a2a
p3rcypj <pablo@percypj.com>
2025-08-09 08:26:23 +0000

    Make CSV locales to JSON

--- src/webapp/pages/app/themes/muiLocales.ts
+++ src/webapp/pages/app/themes/muiLocales.ts
@@ -8,60 +8,51 @@ export type MuiSupportedLocales = keyof typeof locales;
 
 // There is one 'duplication' in the list below, Portuguese (Brazil) and Portuguese (Portugal) are both listed.
 // CSV was done manually just copy-pasting and changing tabs by semicolons.
-const muiLocalesCSV = [
-    "Armenian;hy-AM;hyAM",
-    "Azerbaijani;az-AZ;azAZ",
-    "Bulgarian;bg-BG;bgBG",
-    "Catalan;ca-ES;caES",
-    "Chinese (Simplified);zh-CN;zhCN",
-    "Czech;cs-CZ;csCZ",
-    "Dutch;nl-NL;nlNL",
-    "English (United States);en-US;enUS",
-    "Estonian;et-EE;etEE",
-    "Finnish;fi-FI;fiFI",
-    "French;fr-FR;frFR",
-    "German;de-DE;deDE",
-    "Hebrew;he-IL;heIL",
-    "Hindi;hi-IN;hiIN",
-    "Hungarian;hu-HU;huHU",
-    "Icelandic;is-IS;isIS",
-    "Indonesian;id-ID;idID",
-    "Italian;it-IT;itIT",
-    "Japanese;ja-JP;jaJP",
-    "Korean;ko-KR;koKR",
-    "Persian;fa-IR;faIR",
-    "Polish;pl-PL;plPL",
-    "Portuguese (Brazil);pt-BR;ptBR",
-    "Portuguese;pt-PT;ptPT",
-    "Romanian;ro-RO;roRO",
-    "Russian;ru-RU;ruRU",
-    "Slovak;sk-SK;skSK",
-    "Spanish;es-ES;esES",
-    "Swedish;sv-SE;svSE",
-    "Turkish;tr-TR;trTR",
-    "Ukrainian;uk-UA;ukUA",
-    "Vietnamese;vi-VN;viVN",
-];
+// Later transformed to JSON for easier handling.
+const muiLocales = [
+    { name: "Armenian", bcp47: "hy-AM", importName: "hyAM" },
+    { name: "Azerbaijani", bcp47: "az-AZ", importName: "azAZ" },
+    { name: "Bulgarian", bcp47: "bg-BG", importName: "bgBG" },
+    { name: "Catalan", bcp47: "ca-ES", importName: "caES" },
+    { name: "Chinese (Simplified)", bcp47: "zh-CN", importName: "zhCN" },
+    { name: "Czech", bcp47: "cs-CZ", importName: "csCZ" },
+    { name: "Dutch", bcp47: "nl-NL", importName: "nlNL" },
+    { name: "English (United States)", bcp47: "en-US", importName: "enUS" },
+    { name: "Estonian", bcp47: "et-EE", importName: "etEE" },
+    { name: "Finnish", bcp47: "fi-FI", importName: "fiFI" },
+    { name: "French", bcp47: "fr-FR", importName: "frFR" },
+    { name: "German", bcp47: "de-DE", importName: "deDE" },
+    { name: "Hebrew", bcp47: "he-IL", importName: "heIL" },
+    { name: "Hindi", bcp47: "hi-IN", importName: "hiIN" },
+    { name: "Hungarian", bcp47: "hu-HU", importName: "huHU" },
+    { name: "Icelandic", bcp47: "is-IS", importName: "isIS" },
+    { name: "Indonesian", bcp47: "id-ID", importName: "idID" },
+    { name: "Italian", bcp47: "it-IT", importName: "itIT" },
+    { name: "Japanese", bcp47: "ja-JP", importName: "jaJP" },
+    { name: "Korean", bcp47: "ko-KR", importName: "koKR" },
+    { name: "Persian", bcp47: "fa-IR", importName: "faIR" },
+    { name: "Polish", bcp47: "pl-PL", importName: "plPL" },
+    { name: "Portuguese (Brazil)", bcp47: "pt-BR", importName: "ptBR" },
+    { name: "Portuguese", bcp47: "pt-PT", importName: "ptPT" },
+    { name: "Romanian", bcp47: "ro-RO", importName: "roRO" },
+    { name: "Russian", bcp47: "ru-RU", importName: "ruRU" },
+    { name: "Slovak", bcp47: "sk-SK", importName: "skSK" },
+    { name: "Spanish", bcp47: "es-ES", importName: "esES" },
+    { name: "Swedish", bcp47: "sv-SE", importName: "svSE" },
+    { name: "Turkish", bcp47: "tr-TR", importName: "trTR" },
+    { name: "Ukrainian", bcp47: "uk-UA", importName: "ukUA" },
+    { name: "Vietnamese", bcp47: "vi-VN", importName: "viVN" },
+] as const;
 
-const muiLocalizations = muiLocalesCSV.map(str => {
-    const [name, bcp47, importName] = str.split(";");
+const muiLocalizations = muiLocales.map(localeObj => {
+    const { name, bcp47, importName } = localeObj;
     const localeParts = (bcp47 ?? "").split("-");
     const locale = localeParts[0];
     const country = localeParts[1] ?? localeParts[0]?.toUpperCase();
-
-    if (!isValidImportName(importName)) {
-        throw new Error(`Invalid locale importName: ${importName}`);
-    }
-
     const localization: Localization = locales[importName];
 
     return { name, bcp47, importName, locale, localization, country };
 });
-
-function isValidImportName(importName: Maybe<string>): importName is MuiSupportedLocales {
-    if (!importName) return false;
-    return Object.keys(locales).includes(importName);
-}
 
 export function getMuiLocalization(language: string, country: Maybe<string>): Localization {
     const locale = muiLocalizations.find(l => l.locale === language && l.country === country);
