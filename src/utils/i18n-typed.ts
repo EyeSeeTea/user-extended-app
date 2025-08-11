bcbfa1f00e7595b7e126bf6b0a8bf85c28af128e
p3rcypj <pablo@percypj.com>
2025-08-11 05:35:19 +0000

    Fix webpack complaint

--- src/utils/i18n-typed.ts
+++ src/utils/i18n-typed.ts
@@ -1,7 +1,7 @@
 import i18n from "../locales";
 
 export function getModuleForNamespace(namespace: string): i18n.i18n {
-    const { t: _t, ...rest } = i18n;
+    const { t: _t, changeLanguage, ...rest } = i18n;
 
     const restBinded = Object.fromEntries(
         Object.entries(rest).map(([key, value]) => [key, typeof value === "function" ? value.bind(i18n) : value])
@@ -10,7 +10,8 @@ export function getModuleForNamespace(namespace: string): i18n.i18n {
     return {
         ...restBinded,
         t: function (s: string | string[], options?: i18n.TranslationOptions<object>): string {
-            return i18n.t(s, { ...options, ns: namespace });
+            return i18n.t(s, { ...options, ns: namespace, nsSeparator: ":ns:" });
         },
+        changeLanguage: changeLanguage.bind(i18n),
     };
 }
