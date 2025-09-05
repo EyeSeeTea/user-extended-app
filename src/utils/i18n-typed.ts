1d97720f798d1c3e46993cbbde2eb0c263643662
p3rcypj <pablo@percypj.com>
2025-09-05 16:36:02 +0000

    Override nsSeparator type, generate locales as lib

--- src/utils/i18n-typed.ts
+++ src/utils/i18n-typed.ts
@@ -1,6 +1,7 @@
+import { isBoolean } from "lodash/fp";
 import i18n from "../locales";
 
-export function getModuleForNamespace(namespace: string): i18n.i18n {
+export function getModuleForNamespace(namespace: string) {
     const { t: _t, changeLanguage, ...rest } = i18n;
 
     const restBinded = Object.fromEntries(
@@ -9,9 +10,14 @@ export function getModuleForNamespace(namespace: string): i18n.i18n {
 
     return {
         ...restBinded,
-        t: function (s: string | string[], options?: i18n.TranslationOptions<object>): string {
-            return i18n.t(s, { ...options, ns: namespace });
+        t: function (s: string | string[], options?: i18nOptions): string {
+            const nsSeparator: string | undefined = isBoolean(options?.nsSeparator) ? undefined : options?.nsSeparator;
+            return i18n.t(s, { ...options, ns: namespace, nsSeparator });
         },
         changeLanguage: changeLanguage.bind(i18n),
     };
+}
+
+interface i18nOptions extends Omit<i18n.TranslationOptions<object>, "nsSeparator"> {
+    nsSeparator?: boolean | string;
 }
