ede06f9939e788bae302a0176ddb1172a64c5810
p3rcypj <pablo@percypj.com>
2025-09-05 16:14:30 +0000

    Return nsSeparator to false

--- src/legacy/components/ReplicateUserFromTemplate.component.js
+++ src/legacy/components/ReplicateUserFromTemplate.component.js
@@ -136,7 +136,7 @@ class ReplicateUserFromTemplate extends React.Component {
             const errorMessage = i18n.t("Error replicating user {{user}}: {{message}}", {
                 user: userToReplicate.displayName,
                 message: response.error,
-                nsSeparator: ":ns:",
+                nsSeparator: false,
             });
             snackActions.show({ message: errorMessage });
             onRequestClose();
@@ -157,7 +157,7 @@ class ReplicateUserFromTemplate extends React.Component {
             this.getTextField("username", "string", username, {
                 validators: [this.validators.isValidUsername],
                 label: i18n.t("Username. Example for two users: admin.$index -> admin.1, admin.2", {
-                    nsSeparator: ":ns:",
+                    nsSeparator: false,
                 }),
             }),
             this.getTextField("password", "string", password, {
--- src/webapp/hooks/userHooks.ts
+++ src/webapp/hooks/userHooks.ts
@@ -178,7 +178,7 @@ export const useExportUsers = (props: UseExportUsersProps) => {
                 ({ blob, filename }) => {
                     FileSaver.saveAs(blob, filename);
                     onSuccess();
-                    snackbar.success(i18n.t("Table exported: {{filename}}", { filename, nsSeparator: ":ns:" }));
+                    snackbar.success(i18n.t("Table exported: {{filename}}", { filename, nsSeparator: false }));
                     loading.hide();
                 },
                 error => {
