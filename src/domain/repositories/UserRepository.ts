ea18f3359c1664eca7d71536771cfda8a3b080e6
nshandra <34254522+nshandra@users.noreply.github.com>
2025-10-13 10:11:59 +0200

    feat: add UserIdentifier entity, modify listAllIds, rename ListAllUserIdsUseCase

--- src/domain/repositories/UserRepository.ts
+++ src/domain/repositories/UserRepository.ts
@@ -4,12 +4,13 @@ import { PaginatedResponse } from "../entities/PaginatedResponse";
 import { Id, NamedRef } from "../entities/Ref";
 import { Stats } from "../entities/Stats";
 import { User } from "../entities/User";
+import { UserIdentifier } from "../entities/UserIdentifier";
 
 export interface UserRepository {
     getCurrent(): FutureData<User>;
     list(options: ListOptions): FutureData<PaginatedResponse<User>>;
     listAll(options: ListOptions): FutureData<User[]>;
-    listAllIds(options: ListOptions): FutureData<Id[]>;
+    listAllUserIdentifiers(options: ListOptions): FutureData<UserIdentifier[]>;
     getByIds(ids: Id[]): FutureData<User[]>;
     save(users: User[]): FutureData<MetadataResponse>;
     updateRoles(ids: Id[], update: NamedRef[], strategy: UpdateStrategy): FutureData<MetadataResponse>;
