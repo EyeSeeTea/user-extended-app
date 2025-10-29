a563b57cdb4320d1083e4afe28c3a3bd0e6222e6
Eduardo Peredo Rivero <jrperedo@gmail.com>
2025-10-28 19:43:03 -0500

    allow special chars in username

--- src/domain/value-objects/Username.ts
+++ src/domain/value-objects/Username.ts
@@ -30,16 +30,22 @@ export class Username extends ValueObject<UsernameProps> {
 
         const startError = validateNotRegexp(value, /^[._@-]|[._@-]$/, "Username cannot start or end with a separator");
         const doubleError = validateNotRegexp(value, /([._@-]){2,}/, "Username cannot have two separators in a row");
-        const charError = validateRegexp(
-            value,
-            /^[a-zA-Z0-9._@-]+$/,
-            "Username can only include . _ - or @ as separators"
-        );
+        /* 
+           In the USERS app they have this validation rule, but the API allows these characters
+           and since we have users that already use them, we cannot apply this validation
+           so I'm leaving it commented out for future reference
+        */
+
+        // const charError = validateRegexp(
+        //     value,
+        //     /^[a-zA-Z0-9._@-]+$/,
+        //     "Username can only include . _ - or @ as separators"
+        // );
 
         const minLengthError = validateLengthMin(value, 2, "Username should be at least 2 characters long");
         const maxLengthError = validateLengthMax(value, 255, "Username may not exceed 255 characters");
 
-        const errors = [startError, doubleError, charError, minLengthError, maxLengthError].filter(
+        const errors = [startError, doubleError, minLengthError, maxLengthError].filter(
             error => error !== undefined
         ) as string[];
 
