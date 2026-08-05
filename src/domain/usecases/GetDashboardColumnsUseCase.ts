1dea04020e90f62222315f410fbece1b8c68eafd
Eduardo Peredo Rivero <jrperedo@gmail.com>
2026-08-04 23:24:38 -0500

    refactor columns settings

--- src/domain/usecases/GetDashboardColumnsUseCase.ts
+++ src/domain/usecases/GetDashboardColumnsUseCase.ts
@@ -1,11 +1,9 @@
-import _ from "lodash";
-import { AppSettings, SettingsDashboardColumn } from "../entities/AppSettings";
 import { FutureData } from "../entities/Future";
 import { AppSettingsRepository } from "../repositories/AppSettingsRepository";
-import { isSuperAdmin, UserProps } from "../entities/UserProps";
-import { Maybe } from "../../types/utils";
+import { UserProps } from "../entities/UserProps";
 import { DashboardColumnRepository } from "../repositories/DashboardColumnRepository";
-import { DashboardColumnSetting, DashboardColumnType } from "../entities/DashboardColumn";
+import { DashboardColumnSetting } from "../entities/DashboardColumn";
+import { resolveColumns } from "./common/columns";
 
 export class GetDashboardColumnsUseCase {
     constructor(
@@ -15,67 +13,14 @@ export class GetDashboardColumnsUseCase {
 
     execute(user: UserProps): FutureData<DashboardColumnSetting[]> {
         return this.appSettingsRepository.get().flatMap(appSettings => {
-            return this.getColumnsFromSettings(appSettings, user);
+            return this.dashboardColumnRepository.get().map(dashboardColumnsPreferences => {
+                return resolveColumns({
+                    columnsConfig: appSettings.dashboardColumns,
+                    preferences: dashboardColumnsPreferences,
+                    user: user,
+                    buildColumn: (fieldName, state, position) => ({ fieldName, state, position }),
+                });
+            });
         });
-    }
-
-    private getColumnsFromSettings(appSettings: AppSettings, user: UserProps): FutureData<DashboardColumnSetting[]> {
-        const columnsConfig = appSettings.dashboardColumns;
-
-        return this.dashboardColumnRepository.get().map(roleColumnsPreferences => {
-            const preferencesMap = _.keyBy(roleColumnsPreferences, col => col.fieldName);
-            const isSuperAdminUser = isSuperAdmin(user);
-
-            const columns = _(columnsConfig)
-                .map(columnConfig => {
-                    const existingColumn = preferencesMap[columnConfig.field];
-
-                    return this.processColumnByValue(columnConfig, existingColumn, isSuperAdminUser);
-                })
-                .compact()
-                .orderBy(col => (col.position === -1 ? Infinity : -col.position), "desc")
-                .value();
-
-            return columns;
-        });
-    }
-
-    private processColumnByValue(
-        columnConfig: SettingsDashboardColumn,
-        existingColumn: Maybe<DashboardColumnSetting>,
-        isSuperAdmin: boolean
-    ): Maybe<DashboardColumnSetting> {
-        switch (columnConfig.value) {
-            case "disabled":
-                // Excluded from the result, except for super admins: they get it as an optional column
-                return isSuperAdmin
-                    ? existingColumn ?? this.buildColumn(columnConfig.field, "unselected", -1)
-                    : undefined;
-
-            case "optional":
-                // If exists in preferences, respect its state; otherwise add as unselected
-                return existingColumn ?? this.buildColumn(columnConfig.field, "unselected", -1);
-
-            case "visible":
-                // If exists in preferences, respect its state; otherwise add as selected
-                return existingColumn ?? this.buildColumn(columnConfig.field, "selected", -1);
-
-            case "mandatory": {
-                const mandatoryState = isSuperAdmin ? existingColumn?.state ?? "selected" : "selected-disabled";
-                // Always add as selected-disabled regardless of preferences
-                return this.buildColumn(columnConfig.field, mandatoryState, existingColumn?.position ?? -1);
-            }
-
-            default:
-                return undefined;
-        }
-    }
-
-    private buildColumn(
-        fieldName: DashboardColumnType,
-        state: DashboardColumnSetting["state"],
-        position: number
-    ): DashboardColumnSetting {
-        return { fieldName, state, position };
     }
 }
--- src/domain/usecases/GetGroupColumnsUseCase.ts
+++ src/domain/usecases/GetGroupColumnsUseCase.ts
@@ -1,11 +1,9 @@
-import _ from "lodash";
-import { AppSettings, SettingsGroupColumn } from "../entities/AppSettings";
 import { FutureData } from "../entities/Future";
 import { AppSettingsRepository } from "../repositories/AppSettingsRepository";
-import { isSuperAdmin, UserProps } from "../entities/UserProps";
-import { Maybe } from "../../types/utils";
+import { UserProps } from "../entities/UserProps";
 import { GroupColumnRepository } from "../repositories/GroupColumnRepository";
-import { GroupColumnSetting, GroupColumnType } from "../entities/GroupColumn";
+import { GroupColumnSetting } from "../entities/GroupColumn";
+import { resolveColumns } from "./common/columns";
 
 export class GetGroupColumnsUseCase {
     constructor(
@@ -15,67 +13,14 @@ export class GetGroupColumnsUseCase {
 
     execute(user: UserProps): FutureData<GroupColumnSetting[]> {
         return this.appSettingsRepository.get().flatMap(appSettings => {
-            return this.getColumnsFromSettings(appSettings, user);
+            return this.columnRepository.get().map(groupColumnsPreferences => {
+                return resolveColumns({
+                    columnsConfig: appSettings.groupColumns,
+                    preferences: groupColumnsPreferences,
+                    user: user,
+                    buildColumn: (fieldName, state, position) => ({ fieldName, state, position }),
+                });
+            });
         });
-    }
-
-    private getColumnsFromSettings(appSettings: AppSettings, user: UserProps): FutureData<GroupColumnSetting[]> {
-        const columnsConfig = appSettings.groupColumns;
-
-        return this.columnRepository.get().map(roleColumnsPreferences => {
-            const preferencesMap = _.keyBy(roleColumnsPreferences, col => col.fieldName);
-            const isSuperAdminUser = isSuperAdmin(user);
-
-            const columns = _(columnsConfig)
-                .map(columnConfig => {
-                    const existingColumn = preferencesMap[columnConfig.field];
-
-                    return this.processColumnByValue(columnConfig, existingColumn, isSuperAdminUser);
-                })
-                .compact()
-                .orderBy(col => (col.position === -1 ? Infinity : -col.position), "desc")
-                .value();
-
-            return columns;
-        });
-    }
-
-    private processColumnByValue(
-        columnConfig: SettingsGroupColumn,
-        existingColumn: Maybe<GroupColumnSetting>,
-        isSuperAdmin: boolean
-    ): Maybe<GroupColumnSetting> {
-        switch (columnConfig.value) {
-            case "disabled":
-                // Excluded from the result, except for super admins: they get it as an optional column
-                return isSuperAdmin
-                    ? existingColumn ?? this.buildColumn(columnConfig.field, "unselected", -1)
-                    : undefined;
-
-            case "optional":
-                // If exists in preferences, respect its state; otherwise add as unselected
-                return existingColumn ?? this.buildColumn(columnConfig.field, "unselected", -1);
-
-            case "visible":
-                // If exists in preferences, respect its state; otherwise add as selected
-                return existingColumn ?? this.buildColumn(columnConfig.field, "selected", -1);
-
-            case "mandatory": {
-                const mandatoryState = isSuperAdmin ? existingColumn?.state ?? "selected" : "selected-disabled";
-                // Always add as selected-disabled regardless of preferences
-                return this.buildColumn(columnConfig.field, mandatoryState, existingColumn?.position ?? -1);
-            }
-
-            default:
-                return undefined;
-        }
-    }
-
-    private buildColumn(
-        fieldName: GroupColumnType,
-        state: GroupColumnSetting["state"],
-        position: number
-    ): GroupColumnSetting {
-        return { fieldName, state, position };
     }
 }
